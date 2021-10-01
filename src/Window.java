84b5159a67e8cfb4346262b6c76d24c44429f914
christer Holm <ch@star78.se>
2021-10-01 13:40:09 +0200

    Add Board on Map

--- src/Window.java
+++ src/Window.java
@@ -3,14 +3,15 @@ import javax.swing.*;
 public class Window {
     public static final int WIDTH = 300, HEIGHT = 600;
     private JFrame windows;
-
+private Board board;
     public Window() {
         windows = new JFrame("Tetris");
         windows.setSize(WIDTH, HEIGHT);
         windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         windows.setResizable(false);
         windows.setLocationRelativeTo(null);
-
+board = new Board();
+windows.add(board);
 
         windows.setVisible(true);
     }
