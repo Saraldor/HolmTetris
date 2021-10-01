f61174ebf6cdbadc94357c57581aaab431b8bb4e
christer Holm <ch@star78.se>
2021-10-01 14:47:42 +0200

    Add tiles and board

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,25 @@
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Board  extends JPanel{
-public Board(){
+private BufferedImage blocks;
 
-}
+
+    public Board() {
+
+        try {
+            blocks = ImageIO.read(Board.class.getResource("/textures/tiles.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 public void paintComponent(Graphics g){
  super.paintComponent(g);
- g.drawRect(100,100,50,50);
+g.drawImage(blocks,0,0,null);
 
     }
 }
